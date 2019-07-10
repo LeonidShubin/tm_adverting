cb8a452be0afce267bdf283eaee27511cd21b274
Leonid <leonardoShubin@gmail.com>
2019-07-10 19:03:03 +0300

    1121

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync');
 var useref = require('gulp-useref');
+var uglify = require('gulp-uglify');
 var gulpIf = require('gulp-if');
 var cssnano = require('gulp-cssnano');
 var imagemin = require('gulp-imagemin');
@@ -45,13 +46,14 @@ gulp.task('useref', function() {
 
   return gulp.src('app/*.html')
     .pipe(useref())
+    .pipe(gulpIf('*.js', uglify()))
     .pipe(gulpIf('*.css', cssnano()))
     .pipe(gulp.dest('dist'));
 });
 
 // Optimizing Images 
 gulp.task('images', function() {
-  return gulp.src('app/images/**/*.+(png|jpg|jpeg|gif|svg)')
+  return gulp.src('app/images/**/*.+(png|jpg|jpeg|gif|svg|ico)')
     // Caching images that ran through imagemin
     .pipe(cache(imagemin({
       interlaced: true,
